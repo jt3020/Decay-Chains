72c7c472a00f248fa89491148cfe9c1cd8bc342b
zman950 <zayad.alzayer+github@gmail.com>
2023-06-30 13:51:57 +0100

    added simple endf reader

---
+++ src/endfReader.py
@@ -0,0 +1,29 @@
+# write regex that will find these lines in an endf file
+# Parent half-life: 613.9 S 6                                          1 1451   19
+# Decay Mode: B-                                                       1 1451   20
+import re
+
+def get_half_life(endf_file):
+    """use regex to find the half life number in seconds"""
+    with open(endf_file, 'r') as f:
+        for line in f:
+            if 'Parent half-life' in line:
+                return float(re.findall(r'\d+\.\d+', line)[0])
+    return None
+def get_decay_mode(endf_file):
+    """use regex to find decay mode in endf file"""
+    with open(endf_file, 'r') as f:
+        for line in f:
+            if 'Decay Mode' in line:
+                return re.findall(r'\w+-', line)[0]
+    return None
+#write a function that splits the file name into proton number and atomic symbol and the mass number
+
+def get_atomic_symbol(file_path):
+    """a function that splits the file name into proton number and atomic symbol and the mass number"""
+    file_name = file_path.split('/')[-1]
+    return file_name.split('_')[1]    
+
+print(get_decay_mode('decay_data/dec-000_Nn_001.endf'))
+print(get_half_life('decay_data/dec-000_Nn_001.endf'))
+print(get_atomic_symbol('decay_data/dec-000_Nn_001.endf'))
